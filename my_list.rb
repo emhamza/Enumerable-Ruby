1a2925da352f36b70360953b8c651d2c020d37aa
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-06-16 15:24:16 +0300

    Add updated my_list.rb to fix linther error.

--- my_list.rb
+++ my_list.rb
@@ -16,12 +16,12 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-puts list.all? { |e| e < 5 } 
-puts list.all? { |e| e > 5 } 
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
 # Test #any?
-puts list.any? { |e| e == 2 } 
-puts list.any? { |e| e == 5 } 
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
 # Test #filter
-puts list.filter { |e| e.even? }
+puts(list.filter(&:even?))
