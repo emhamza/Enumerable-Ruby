e6e0d4975eaeb8cc298bf97f7605619f42ed01b8
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-06-16 15:20:37 +0300

    Add update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -11,3 +11,17 @@ class MyList
     @list.each(&block)
   end
 end
+
+# Test cases
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? { |e| e < 5 } 
+puts list.all? { |e| e > 5 } 
+
+# Test #any?
+puts list.any? { |e| e == 2 } 
+puts list.any? { |e| e == 5 } 
+
+# Test #filter
+puts list.filter { |e| e.even? }
